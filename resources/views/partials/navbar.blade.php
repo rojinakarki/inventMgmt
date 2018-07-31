5ce27ddc03afd1fe309753266293229a75aa653c
rojinakarki <rojinaa.karkii@gmail.com>
2018-07-31 12:09:21 +0545

    Initial Commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,6 @@
+<li><a href="{{url('usergroup')}}">User Group</a></li>
+<li><a href="{{url('organization')}}">Organization</a></li>
+<li><a href="{{url('users')}}">Users</a></li>
+<li><a href="{{url('package')}}">Package</a></li>
+<li><a href="{{url('course')}}">Course</a></li>
+<li><a href="{{url('lesson')}}">Lesson</a></li>
